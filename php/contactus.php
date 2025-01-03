b7590eb7bc71deda4f0cae3e5b506d3531dd5571
DonikaNuredinii <donikanuredini3@gmail.com>
2025-01-03 15:26:48 +0100

    contact-us page

---
+++ php/contactus.php
@@ -0,0 +1,69 @@
+<?php include 'header.php'; ?>
+<div class="CUsection">
+    <div class="introCU">
+      <div class="intro1Cu">
+        <h2>Reach Out For Any Information</h2>
+        <p>
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly
+          believable.
+        </p>
+      </div>
+    </div>
+    <div class="partOneCu">
+      <div class="rightPartCu">
+        <div class="listC">
+          <ul>
+            <li class="h">Opening Hours</li>
+            <li>Monday - Friday</li>
+            <li>9am - 7pm</li>
+            <li>Weekend</li>
+            <li>Closed</li>
+          </ul>
+          <ul>
+            <li class="h">Address</li>
+            <li>Rruga Vëllezërit Gërvalla, Ferizaj</li>
+          </ul>
+          <ul>
+            <li class="h">Contact Details</li>
+            <li>S&OLux@gmail.com</li>
+            <li>+383 45 123 321</li>
+          </ul>
+        </div>
+      </div>
+      <div class="leftPartCu">
+        <h1>Contact Us</h1>
+        <form>
+          <p class="pcon">
+            Feel free to contact us and we will get back to you as soon as we
+            can!
+          </p>
+          <div class="inputs-contact">
+            <input type="text" placeholder="Your Name.." aria-label="Name" />
+          </div>
+          <div class="inputs-contact">
+            <input type="text" placeholder="E-mail" aria-label="Email" />
+          </div>
+          <div class="inputs-contact-Message">
+            <textarea placeholder="Tell us all about it" aria-label="Message"></textarea>
+          </div>
+          <button type="submit" class="button-CU">Send</button>
+        </form>
+      </div>
+    </div>
+    <div class="mapCu">
+      <h2>Find Our Location Below</h2>
+      <div class="mapcomponant">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11787.731233616466!2d21.15237823001321!3d42.67011596072585!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13549de0c1d9b64b%3A0xf06c20ff0111a223!2sPristina!5e0!3m2!1sen!2s!4v1698495211111!5m2!1sen!2s"
+          width="900"
+          height="400"
+          style="border:0;"
+          allowfullscreen=""
+          loading="lazy"
+        ></iframe>
+      </div>
+    </div>
+  </div>
+  <?php include 'footer.php'; ?>
